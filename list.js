740493e8ebf801409a1bc3bb78c17f228222d092
Dafu Ai <dafua@student.unimelb.edu.au>
2019-05-02 13:54:57 +1000

    update back end

--- list.js
+++ list.js
@@ -2,24 +2,27 @@ import * as squel from "squel";
 import {badRequest, failure, success} from "./libs/response-lib";
 import * as athena from "./libs/athena-lib";
 
-const rowsPerPage = 20;
+const DEFAULT_ROWS_PER_PAGE = 20;
+const SORTABLE_COLUMNS = ['size', 'last_modified_date'];
 
 export const main = async (event, context) => {
-    const searchString = event.queryStringParameters.query;
+    const queryStringParams = event.queryStringParameters;
+    const filePath = queryStringParams.filePath;
 
-    if (searchString === null) {
+    if (filePath === null) {
         return badRequest({errors: 'query cannot be empty'});
     }
 
-    const pageString = event.queryStringParameters.page;
+    const pageString = queryStringParams.page;
     // Default index is 0
     const page = pageString ? parseInt(pageString) : 0;
+    const rowsPerPageString = queryStringParams.rowsPerPage;
+    const rowsPerPage = rowsPerPageString ? parseInt(rowsPerPageString) : DEFAULT_ROWS_PER_PAGE;
 
-    const innerQueryBuilder = squel
-        .select()
-        .field('row_number() over()', 'rn')
-        .field('*')
-        .from('data');
+    const sortCol = queryStringParams.sortCol;
+    const sortAsc = queryStringParams.sortAsc === true || queryStringParams.sortAsc === 'true';
+
+    const sortQueryString = sortCol ? `ORDER BY ${sortCol} ${sortAsc ? 'ASC' : 'DESC'}` : '';
 
     const rowStart = rowsPerPage * page + 1;
     const rowEnd = rowsPerPage * (page+1);
@@ -30,12 +33,26 @@ export const main = async (event, context) => {
 
     const searchExpression = squel
         .expr()
-        .and(`key like '%${searchString}%'`);
+        .and(`key like '%${filePath}%'`);
+
+    if (queryStringParams.fileExtension) {
+        searchExpression.and(`key like '%.${queryStringParams.fileExtension}'`);
+    }
+
+    const innerQuery = squel
+        .select()
+        .field(`row_number() over(${sortQueryString})`, 'rn')
+        .field('bucket')
+        .field('key')
+        .field('size')
+        .field('last_modified_date')
+        .from('data')
+        .where(searchExpression);
 
     const dataQuery = squel
         .select()
-        .from(innerQueryBuilder)
-        .where(squel.expr().and(paginationExpression).and(paginationExpression));
+        .from(innerQuery)
+        .where(paginationExpression);
 
     const metaDataQuery = squel
         .select()
@@ -65,7 +82,14 @@ export const main = async (event, context) => {
 };
 
 const processResultRows = rows => {
-    const headerRow = rows[0].Data.map(col => col.VarCharValue);
+    const headerRow = rows[0].Data.map(col => {
+        const val = col.VarCharValue;
+
+        return {
+            key: val,
+            sortable: SORTABLE_COLUMNS.includes(val)
+        }
+    });
     const dataRows = rows.slice(1).map(row => row.Data.map(col => col.VarCharValue));
     return { headerRow, dataRows };
 };
