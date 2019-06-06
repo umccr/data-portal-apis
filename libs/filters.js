3d38dd29fbe65a0b93e5fe5ab14ba64c1eab70e6
Dafu Ai <dafua@student.unimelb.edu.au>
2019-06-06 15:15:57 +1000

    added case sensitivity support for file search

--- libs/filters.js
+++ libs/filters.js
@@ -22,38 +22,49 @@ const FILTER_COLUMNS = {
     }
 };
 
+const FILTER_TYPE_END_WITH = 'end_with';
+const FILTER_TYPE_INCLUDE = 'include';
+const FILTER_TYPE_COMPARE = 'compare';
+const FILTER_TYPE_GLOBAL = 'global';
+
 const FILTERS = {
     default: 'key.include',
     filters: [
         {
             column: 'key',
-            type: 'end_with',
+            type: FILTER_TYPE_END_WITH,
             alias: 'ext',
             description: 'File extension'
         },
         {
             column: 'key',
-            type: 'include',
+            type: FILTER_TYPE_INCLUDE,
             alias: 'pathinc',
             description: 'File path includes'
         },
         {
             column: 'size',
-            type: 'compare',
+            type: FILTER_TYPE_COMPARE,
             alias: 'size',
             description: 'Compare with file size'
         },
         {
             column: 'last_modified_date',
-            type: 'compare',
+            type: FILTER_TYPE_COMPARE,
             alias: 'date',
             description: 'Compare with last modified date of the file'
         },
         {
             column: 'illumina_id',
-            type: 'include',
+            type: FILTER_TYPE_INCLUDE,
             alias: 'illumina_id',
-            descriptionL: 'Illumina_id (in LIMS table) includes'
+            description: 'Illumina_id (in LIMS table) includes'
+        },
+        {
+            column: 'case',
+            type: FILTER_TYPE_GLOBAL,
+            alias: 'case',
+            description: 'Defines case sensitivity for string comparison. Default to false'
         }
     ],
 };
@@ -116,13 +127,43 @@ const get_key_patterns_from_lims = async illumina_id => {
     return patterns;
 };
 
-const getExpressionFromFilter = async (filterKey, filterVal) => {
+const LIKE_INCLUDE = 'include';
+const LIKE_START_WITH = 'start_with';
+const LIKE_END_WITH = 'end_with';
+const LIKE_EQUAL = 'equal';
+
+const getLikeExpression = (col, val, mode, caseSensitive) => {
+    let wrappedVal;
+
+    switch (mode) {
+        case LIKE_INCLUDE:
+            wrappedVal = `%${val}%`;
+            break;
+        case LIKE_START_WITH:
+            wrappedVal = `${val}%`;
+            break;
+        case LIKE_END_WITH:
+            wrappedVal = `%${val}`;
+            break;
+        case LIKE_EQUAL:
+            wrappedVal = val;
+    }
+
+    wrappedVal = `'${wrappedVal}'`
+
+    const formattedCol = caseSensitive ? col : `LOWER(${col})`;
+    const formattedVal = caseSensitive ? wrappedVal : `LOWER(${wrappedVal})`;
+
+    return `${formattedCol} LIKE ${formattedVal}`;
+};
+
+const getExpressionFromFilter = async (filterKey, filterVal, isCaseSensitive) => {
     const filter = findFilter(filterKey);
 
     const exp = squel.expr();
 
     switch (filter.type) {
-        case "compare":
+        case FILTER_TYPE_COMPARE:
             const {operator, valToCompare} = decomposeComparisonVal(filterVal);
             let wrappedVal = valToCompare;
 
@@ -133,10 +174,10 @@ const getExpressionFromFilter = async (filterKey, filterVal) => {
 
             exp.and(`${filter.column} ${operator} ${wrappedVal}`);
             break;
-        case "end_with":
-            exp.and(`${filter.column} LIKE \'%${filterVal}\'`);
+        case FILTER_TYPE_END_WITH:
+            exp.and(getLikeExpression(filter.column, filterVal, LIKE_END_WITH, isCaseSensitive));
             break;
-        case "include":
+        case FILTER_TYPE_INCLUDE:
             // Special case for LIMS Illumina filtering
             if (FILTER_COLUMNS[filter.column].type === COLUMN_TYPE_ILLUMINA) {
                 const patterns = await get_key_patterns_from_lims(filterVal);
@@ -149,12 +190,13 @@ const getExpressionFromFilter = async (filterKey, filterVal) => {
 
                 // We only need at least one pattern to be matched
                 for (let i=0; i<patterns.length; i++) {
-                    innerExp.or(`key LIKE \'%${patterns[i]}%\'`)
+                    innerExp.or(getLikeExpression(key, patterns[i], LIKE_INCLUDE, isCaseSensitive))
                 }
 
                 exp.and(innerExp);
             } else {
-                exp.and(`${filter.column} LIKE \'%${filterVal}%\'`);
+                // Normal include case
+                exp.and(getLikeExpression(filter.column, filterVal, LIKE_INCLUDE, isCaseSensitive))
             }
 
             break;
@@ -167,26 +209,51 @@ const getExpressionFromFilter = async (filterKey, filterVal) => {
 
 const parseFilterQueryString = async queryString => {
     const filters = queryString.trim().split(' ');
-
     const exp = squel.expr();
+    const filterConfigs = [];
 
+    // Check through all filter strings first
     for (let i=0; i<filters.length; i++) {
         const filter = filters[i];
         const tokens = filter.split(':');
-        let filterKey;
+        let filterKey, filterVal;
 
         switch (tokens.length) {
             case 1:
+                // case {filterVal} (default filter)
                 filterKey = FILTERS.default;
-                exp.and(await getExpressionFromFilter(filterKey, tokens[0]));
+                filterVal = tokens[0];
                 break;
             case 2:
+                // case {filterKey}:{filterVal}
                 filterKey = tokens[0];
-                exp.and(await getExpressionFromFilter(filterKey, tokens[1]));
+                filterVal = tokens[1];
                 break;
             default:
                 throw `Unexpected token in ${filter}`;
         }
+
+        // Append filter config
+        filterConfigs.push({
+            key: filterKey,
+            val: filterVal
+        });
+    }
+
+    // Find config for case sensitivity first, if it is true, then it is case sensitive; default to false
+    const caseSensitivityFilterIndex = filterConfigs.findIndex(f => f.key === 'case');
+    const isCaseSensitive = caseSensitivityFilterIndex >= 0
+        && filterConfigs[caseSensitivityFilterIndex].val === 'true';
+
+    for (let i=0; i<filterConfigs.length; i++) {
+        if (i === caseSensitivityFilterIndex) {
+            // Ignore global filter
+            continue;
+        }
+
+        const filterObject = filterConfigs[i];
+        // Wait til filter has been loaded and append this to our filter expression
+        exp.and(await getExpressionFromFilter(filterObject.key, filterObject.val, isCaseSensitive));
     }
 
     return exp;
