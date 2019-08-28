a6e75c240574c9212a42e6bd02943756d8674482
Dafu Ai <dafua@student.unimelb.edu.au>
2019-08-29 09:26:23 +1000

    Added code comments

--- utils/ssm.py
+++ utils/ssm.py
@@ -4,6 +4,11 @@ client = boto3.client('ssm')
 
 
 def get_secret(key):
+    """
+    Retrieve the secret value from SSM.
+    :param key: the key of the secret
+    :return: the secret value
+    """
     resp = client.get_parameter(
         Name=key,
         WithDecryption=True
