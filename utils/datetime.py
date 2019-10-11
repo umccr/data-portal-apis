f00f589c4bae1480beb568e0f1320093469a60ac
Roman Valls Guimera <brainstorm@nopcode.org>
2019-10-11 14:50:39 +1100

    Turn LIMS date format into ISO8601

--- utils/datetime.py
+++ utils/datetime.py
@@ -6,4 +6,4 @@ def parse_last_modified_date(date_raw: str):
 
 
 def parse_lims_timestamp(date_raw: str):
-    return datetime.strptime(date_raw, '%d/%m/%y')
+    return datetime.strptime(date_raw, '%Y-%m-%d')
