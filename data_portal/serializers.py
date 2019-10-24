2f96fb3058334e6b77fce829178d2ad579bf21b0
Dafu Ai <dafua@student.unimelb.edu.au>
2019-10-24 16:49:05 +1100

    Expand lims column

--- data_portal/serializers.py
+++ data_portal/serializers.py
@@ -1,7 +1,7 @@
-from collections import defaultdict
 from typing import Dict, List
 
 from rest_framework import serializers
+from rest_framework.fields import empty
 
 from data_portal.models import S3Object, LIMSRow
 
@@ -9,9 +9,11 @@ from data_portal.models import S3Object, LIMSRow
 class LIMSRowSerializer(serializers.Serializer):
     illumina_id = serializers.CharField()
     run = serializers.IntegerField()
-    subject_id = serializers.CharField()
-    external_subject_id = serializers.CharField()
+    subject_ids = serializers.SerializerMethodField()
     sample_id = serializers.CharField()
+
+    def get_subject_ids(self, obj: LIMSRow) -> str:
+        return f'{obj.subject_id}/{obj.external_subject_id}'
 
     def update(self, instance, validated_data):
         pass
@@ -28,7 +30,14 @@ class S3ObjectSerializer(serializers.Serializer):
     path = serializers.SerializerMethodField()
     size = serializers.IntegerField()
     last_modified_date = serializers.DateTimeField()
-    lims = serializers.SerializerMethodField()
+    illumina_ids = serializers.SerializerMethodField()
+    runs = serializers.SerializerMethodField()
+    subject_ids = serializers.SerializerMethodField()
+    sample_ids = serializers.SerializerMethodField()
+
+    def __init__(self, instance=None, data=empty, **kwargs):
+        super().__init__(instance, data, **kwargs)
+        self.lims = None
 
     def get_rn(self, obj: S3Object) -> int:
         """
@@ -42,14 +51,14 @@ class S3ObjectSerializer(serializers.Serializer):
         """
         return 's3://%s/%s' % (obj.bucket, obj.key)
 
-    def get_lims(self, obj: S3Object) -> Dict:
+    def get_lims(self, obj: S3Object):
         """
-        Get associated LIMS data
-        :return: list of LIMS data
+        Get and set associated LIMS data, so it can be used for other lims field getters
         """
         lims_rows = LIMSRow.objects.filter(s3lims__s3_object=obj)
 
-        field_value_list = defaultdict(list)
+        # In case we don't have associated lims record
+        field_value_list = {'illumina_id': [], 'run': [], 'subject_ids': [], 'sample_id': []}
         for lims_row in lims_rows:
             serializer = LIMSRowSerializer(instance=lims_row)
             lims_row_data = serializer.data
@@ -57,11 +66,48 @@ class S3ObjectSerializer(serializers.Serializer):
             for field, value in lims_row_data.items():
                 field_value_list[field].append(str(value))
 
+        # Concatenate field value lists
         concatenated_data = {}
         for field, value_list in field_value_list.items():
             concatenated_data[field] = ','.join(value_list)
 
-        return concatenated_data
+        self.lims = concatenated_data
+
+    def get_illumina_ids(self, obj: S3Object):
+        """
+        Get concatenated illumina ids
+        """
+        if self.lims is None:
+            self.get_lims(obj)
+
+        return self.lims['illumina_id']
+
+    def get_runs(self, obj: S3Object):
+        """
+        Get concatenated run numbers
+        """
+        if self.lims is None:
+            self.get_lims(obj)
+
+        return self.lims['run']
+
+    def get_subject_ids(self, obj: S3Object):
+        """
+        Get concatenated subject ids
+        """
+        if self.lims is None:
+            self.get_lims(obj)
+
+        return self.lims['subject_ids']
+
+    def get_sample_ids(self, obj: S3Object):
+        """
+        Get concatenated sample ids
+        """
+        if self.lims is None:
+            self.get_lims(obj)
+
+        return self.lims['sample_id']
 
     def to_representation(self, instance: S3Object) -> list:
         """
