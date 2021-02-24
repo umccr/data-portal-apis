53d8c86f7e6817adfcb116a0e2e86099bf6804df
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2021-02-24 13:17:23 +1100

    Add pre-signed URL retrieval

--- data_processors/pipeline/lambdas/gds_search.py
+++ data_processors/pipeline/lambdas/gds_search.py
@@ -7,7 +7,8 @@ import os
 import django
 import logging
 from data_processors.pipeline import services
-from utils import libjson
+from libiap.openapi import libgds
+from utils import libjson, iap
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'data_portal.settings.base')
 django.setup()
@@ -16,9 +17,19 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
-def get_presigned_url(volume: str, file: str):
-    # TODO: implement
-    return f"https://fake.com/presigned/{volume}/{file}"
+def get_presigned_url(file_id: str):
+    logger.debug(f"Retrieving pre-signed URL for file: {file_id}")
+    with libgds.ApiClient(iap.configuration(libgds)) as api_client:
+        file_api = libgds.FilesApi(api_client)
+    try:
+        file_details = libgds.FileListResponse = file_api.get_file(file_id=file_id)
+    except libgds.ApiException as e:
+        logger.info("Exception when calling FilesApi: %s\n" % e)
+
+    print("FILE DETAILS")
+    print(file_details)
+
+    return file_details.presigned_url
 
 
 def handler(event, context):
@@ -66,13 +77,12 @@ def handler(event, context):
     if not query_set.exists():
         logger.error(f"No GDS file records found for search criteria: {libjson.dumps(event)}")
 
-    results = {'volume': gds_volume_name,
-               'files': []}
+    results = {'volume': gds_volume_name, 'files': []}
     for record in query_set:
-        results['files'].append({'path': record.path})
+        results['files'].append({'path': record.path, 'id': record.file_id})
 
     if presigned:
         for file in results['files']:
-            file['presigned_url'] = get_presigned_url(volume=gds_volume_name, file=file['path'])
+            file['presigned_url'] = get_presigned_url(file_id=file['id'])
 
     return results
