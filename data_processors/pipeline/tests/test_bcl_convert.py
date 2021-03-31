ffd880c686dd15b09174c4575f03343df4a7cce1
Victor San Kho Lin <victor@sankholin.com>
2021-03-31 14:59:05 +1100

    Fixed bcl convert test case input template mock

--- data_processors/pipeline/tests/test_bcl_convert.py
+++ data_processors/pipeline/tests/test_bcl_convert.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 from django.utils.timezone import make_aware
 from libiap.openapi import libwes
-from mockito import when, verify
+from mockito import when, verify, contains
 
 from data_portal.models import SequenceRun, Workflow
 from data_portal.tests.factories import SequenceRunFactory, TestConstant
@@ -33,27 +33,38 @@ class BCLConvertUnitTests(PipelineUnitTestCase):
             }
         )
 
+        # This introspect `libssm` module import in `bcl_convert` lambda module
+        # and patch `get_ssm_param()` function argument equal to `wfl_helper.get_ssm_key_input()`
+        # i.e. /iap/workflow/bcl_convert/input.
+        # Then instead of retrieving from actual SSM parameter store, it return the mock template in JSON string
+        #
+        # NOTE:
+        # Ideally we shouldn't mock this. because we also wanted to test such that someone would go directly modify
+        # SSM parameter without changing corresponding code in Portal -- we want to capture that during Continuous
+        # Integration (CI) build phase and before deploying to target environment (Continuous Delivery (CD))
+        # Leaving here for showing mock construct and ad-hoc test use case. Perhaps keep it commented out when no use.
         wfl_helper = WorkflowHelper(WorkflowType.BCL_CONVERT.value)
-
-        when(wfl_helper.get_ssm_key_input()).thenReturn(
-            {
-                "bcl_input_directory": {
-                    "class": "Directory",
-                    "location": "PLACEHOLDER"
-                },
-                "samplesheet": {
-                    "class": "File",
-                    "location": "PLACEHOLDER"
-                },
-                "settings_by_samples": [],
-                "samplesheet_outdir": "samplesheets-by-assay-type",
-                "ignore_missing_samples": True,
-                "samplesheet_output_format": "v2",
-                "bcl_sampleproject_subdirectories_bcl_conversion": True,
-                "strict_mode_bcl_conversion": True,
-                "delete_undetermined_indices_bcl_conversion": True,
-                "runfolder_name": "PLACEHOLDER"
-            }
+        when(bcl_convert.libssm).get_ssm_param(contains(wfl_helper.get_ssm_key_input())).thenReturn(
+            json.dumps(
+                {
+                    "bcl_input_directory": {
+                        "class": "Directory",
+                        "location": "PLACEHOLDER"
+                    },
+                    "samplesheet": {
+                        "class": "File",
+                        "location": "PLACEHOLDER"
+                    },
+                    "settings_by_samples": [],
+                    "samplesheet_outdir": "samplesheets-by-assay-type",
+                    "ignore_missing_samples": True,
+                    "samplesheet_output_format": "v2",
+                    "bcl_sampleproject_subdirectories_bcl_conversion": True,
+                    "strict_mode_bcl_conversion": True,
+                    "delete_undetermined_indices_bcl_conversion": True,
+                    "runfolder_name": "PLACEHOLDER"
+                }
+            )
         )
 
     def test_handler(self):
