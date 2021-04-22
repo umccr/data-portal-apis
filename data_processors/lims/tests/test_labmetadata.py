a07d2bd133c56478683d3d6ca3f269741714d3f5
Andrei Seleznev <aseleznev@illumina.com>
2021-04-22 11:12:38 +1000

    clean up labmetadata ingestion tests

--- data_processors/lims/tests/test_labmetadata.py
+++ data_processors/lims/tests/test_labmetadata.py
@@ -29,23 +29,17 @@ LIB04,SAMIDA-EXTSAMA,SAMIDA,EXTSAMA,SUBIDA,EXTSUBIDA,tumor,poor,FFPE,MyPath,Alic
 def _generate_labmetadata_row_dict(id: str) -> dict:
     """
     Generate labmetadata row dict
-    :param id: this just gets used asa suffix
+    :param id: this just gets used as a suffix
     :return: row dict
     """
 
-    # 'clean' CSV columns into django model compatible columns
+    # 'clean' CSV columns into columns matching django model  
     df = pd.DataFrame(columns=labmetadata_csv_columns)
     cleaned_labmetadata_csv_columns = list(labmetadata.clean_labmetadata_dataframe_columns(df).columns.values)
 
     row = dict()
     for col in cleaned_labmetadata_csv_columns:
-        if col == 'run':
-            row[col] = '1'
-        elif col == 'timestamp':
-            row[col] = '2019-01-01'
-        else:
-            # Normal columns, just use column name as value + id
-            row[col] = col + id
+        row[col] = col + id
     return row
 
 
@@ -72,7 +66,7 @@ class LimsUnitTests(LimsUnitTestCase):
         mock_labmetadata_sheet.seek(0)
         mock_labmetadata_sheet.flush()
         
-        # make a duplicate , its phenotype is normal, in sheet it is tumor
+        # make a duplicate to test update, its phenotype is normal but in sheet it is tumor
         test_existing_sample=LabMetadata.objects.create(library_id='LIB03',sample_name='SAMIDB-EXTSAMB',sample_id='SAMIDB',external_sample_id='EXTSAMB',subject_id='SUBIDB',external_subject_id='EXTSUBIDB',phenotype='NORMAL',quality='poor',source='FFPE',project_name='Fake',project_owner='Bob',experiment_id='Exper1',type='WTS',assay='NebRNA',override_cycles='Y151;I8;I8;Y151',workflow='clinical',coverage='6.0',truseqindex='H07')
 
         # print csv file in tmp dir -- if delete=False, you can see the mock csv content
@@ -110,7 +104,6 @@ class LimsUnitTests(LimsUnitTestCase):
         self.assertEqual(LabMetadata.objects.get(subject_id='subject_id1', sample_name=sample_name).external_sample_id, row_1['external_sample_id'])
         self.assertEqual(process_results['labmetadata_row_update_count'], 0)
         self.assertEqual(process_results['labmetadata_row_invalid_count'], 0)
-
    
     def test_labmetadata_update(self) -> None:
         row_1 = _generate_labmetadata_row_dict('1')
@@ -129,7 +122,7 @@ class LimsUnitTests(LimsUnitTestCase):
     def test_labmetadata_row_duplicate(self) -> None:
         row_duplicate = _generate_labmetadata_row_dict('3')
         process_results = persist_labmetadata(_generate_labmetadata_df([row_duplicate,row_duplicate]))
-        logger.info(json.dumps(process_results))
+        
         self.assertEqual(process_results['labmetadata_row_update_count'], 0)
         self.assertEqual(process_results['labmetadata_row_new_count'], 1)
         self.assertEqual(process_results['labmetadata_row_invalid_count'], 1)
@@ -137,17 +130,25 @@ class LimsUnitTests(LimsUnitTestCase):
     def test_labmetadata_non_nullable_columns(self) -> None:
         row_1 = _generate_labmetadata_row_dict('1')
         row_2 = _generate_labmetadata_row_dict('2')
+        row_3 = _generate_labmetadata_row_dict('3')
+        row_4 = _generate_labmetadata_row_dict('4') 
+        row_5 = _generate_labmetadata_row_dict('5')
+        row_6 = _generate_labmetadata_row_dict('6')
 
-        # Use blank values for all non-nullable fields
         row_1['sample_id'] = ''
         row_1['sample_name'] = ''
         row_1['library_id'] = ''
-        # the bad row should be removed from the DF by persist_labmetada to end with 1 new and 1 invalid
-        process_results = persist_labmetadata(_generate_labmetadata_df([row_1, row_2]))
+        row_3['sample_name'] = ''
+        row_4['sample_id'] = ''
+        row_5['library_id'] = ''
+        
+        process_results = persist_labmetadata(_generate_labmetadata_df([row_1, row_2, row_3, row_4, row_5, row_6, row_6]))
+        
         self.assertEqual(process_results['labmetadata_row_update_count'], 0)
-        self.assertEqual(process_results['labmetadata_row_new_count'], 1)
-        self.assertEqual(process_results['labmetadata_row_invalid_count'], 1)
-        self.assertEqual(LabMetadata.objects.count(), 1)
+        self.assertEqual(process_results['labmetadata_row_new_count'], 2) # row_2 and one of row_6 duplicates
+        self.assertEqual(process_results['labmetadata_row_invalid_count'], 5)
+        
+        self.assertEqual(LabMetadata.objects.count(), 2)
 
     def test_labmetadata_empty_colums(self) -> None:
         # set a bunch of nullable/empty-able rows 
@@ -174,12 +175,12 @@ class LimsIntegrationTests(LimsIntegrationTestCase):
     @skip
     def test_scheduled_update_handler(self):
         """
-        python manage.py test data_processors.lims.tests.test_google_lims.LimsIntegrationTests.test_scheduled_update_handler
+        python manage.py test data_processors.lims.tests.test_labmetadata.LimsIntegrationTests.test_scheduled_update_handler
         """
         result = labmetadata.scheduled_update_handler({'event': "LimsIntegrationTests lims update event"}, None)
 
         logger.info("-" * 32)
-        logger.info("Example google_lims.scheduled_update_handler lambda output:")
+        logger.info("Example LimsIntegrationTests.scheduled_update_handler lambda output:")
         logger.info(json.dumps(result))
         self.assertGreater(result['labmetadata_row_new_count'], 1)
 
