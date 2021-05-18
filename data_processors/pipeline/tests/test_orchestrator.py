70f69a0715a308e31386e15b8807c1c3beb9f30b
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2021-05-18 12:55:52 +1000

    Add test to check T/N job format

--- data_processors/pipeline/tests/test_orchestrator.py
+++ data_processors/pipeline/tests/test_orchestrator.py
@@ -16,6 +16,53 @@ from data_processors.pipeline.tests import _rand
 from data_processors.pipeline.tests.case import logger, PipelineUnitTestCase, PipelineIntegrationTestCase
 
 
+tn_mock_subject_id = "SBJ00001"
+tn_mock_normal_read_1 = "gds://volume/path/normal_read_1.fastq.gz"
+
+
+def build_tn_mock():
+    mock_wfl_run = libwes.WorkflowRun()
+    mock_wfl_run.id = TestConstant.wfr_id.value
+    mock_wfl_run.status = WorkflowStatus.SUCCEEDED.value
+    mock_wfl_run.time_stopped = make_aware(datetime.utcnow())
+    mock_wfl_run.output = {}
+    workflow_version: libwes.WorkflowVersion = libwes.WorkflowVersion()
+    workflow_version.id = TestConstant.wfv_id.value
+    mock_wfl_run.workflow_version = workflow_version
+    when(libwes.WorkflowRunsApi).get_workflow_run(...).thenReturn(mock_wfl_run)
+
+    mock_labmetadata_normal = LabMetadata()
+    mock_labmetadata_normal.subject_id = tn_mock_subject_id
+    mock_labmetadata_normal.library_id = TestConstant.library_id_normal.value
+    mock_labmetadata_normal.phenotype = LabMetadataPhenotype.NORMAL.value
+    mock_labmetadata_normal.type = LabMetadataType.WGS.value
+    mock_labmetadata_normal.save()
+
+    mock_labmetadata_tumor = LabMetadata()
+    mock_labmetadata_tumor.subject_id = tn_mock_subject_id
+    mock_labmetadata_tumor.library_id = TestConstant.library_id_tumor.value
+    mock_labmetadata_tumor.phenotype = LabMetadataPhenotype.TUMOR.value
+    mock_labmetadata_tumor.type = LabMetadataType.WGS.value
+    mock_labmetadata_tumor.save()
+
+    mock_flr_normal = FastqListRow()
+    mock_flr_normal.lane = 1
+    mock_flr_normal.rglb = TestConstant.library_id_normal.value
+    mock_flr_normal.rgsm = TestConstant.sample_id.value
+    mock_flr_normal.rgid = str(uuid.uuid4())
+    mock_flr_normal.read_1 = tn_mock_normal_read_1
+    mock_flr_normal.save()
+
+    mock_flr_tumor = FastqListRow()
+    mock_flr_tumor.lane = 2
+    mock_flr_tumor.rglb = TestConstant.library_id_tumor.value
+    mock_flr_tumor.rgsm = TestConstant.sample_id.value
+    mock_flr_tumor.rgid = str(uuid.uuid4())
+    mock_flr_tumor.read_1 = "gds://volume/path/tumor_read_1.fastq.gz"
+    mock_flr_tumor.read_2 = "gds://volume/path/tumor_read_2.fastq.gz"
+    mock_flr_tumor.save()
+
+
 class OrchestratorUnitTests(PipelineUnitTestCase):
 
     def test_parse_bcl_convert_output(self):
@@ -242,47 +289,7 @@ class OrchestratorUnitTests(PipelineUnitTestCase):
 
         mock_germline_workflow: Workflow = GermlineWorkflowFactory()
 
-        mock_wfl_run = libwes.WorkflowRun()
-        mock_wfl_run.id = TestConstant.wfr_id.value
-        mock_wfl_run.status = WorkflowStatus.SUCCEEDED.value
-        mock_wfl_run.time_stopped = make_aware(datetime.utcnow())
-        mock_wfl_run.output = {}
-
-        workflow_version: libwes.WorkflowVersion = libwes.WorkflowVersion()
-        workflow_version.id = TestConstant.wfv_id.value
-        mock_wfl_run.workflow_version = workflow_version
-        when(libwes.WorkflowRunsApi).get_workflow_run(...).thenReturn(mock_wfl_run)
-
-        mock_labmetadata_normal = LabMetadata()
-        mock_labmetadata_normal.subject_id = "SBJ00001"
-        mock_labmetadata_normal.library_id = TestConstant.library_id_normal.value
-        mock_labmetadata_normal.phenotype = LabMetadataPhenotype.NORMAL.value
-        mock_labmetadata_normal.type = LabMetadataType.WGS.value
-        mock_labmetadata_normal.save()
-
-        mock_labmetadata_tumor = LabMetadata()
-        mock_labmetadata_tumor.subject_id = "SBJ00001"
-        mock_labmetadata_tumor.library_id = TestConstant.library_id_tumor.value
-        mock_labmetadata_tumor.phenotype = LabMetadataPhenotype.TUMOR.value
-        mock_labmetadata_tumor.type = LabMetadataType.WGS.value
-        mock_labmetadata_tumor.save()
-
-        mock_flr_normal = FastqListRow()
-        mock_flr_normal.lane = 1
-        mock_flr_normal.rglb = TestConstant.library_id_normal.value
-        mock_flr_normal.rgsm = TestConstant.sample_id.value
-        mock_flr_normal.rgid = str(uuid.uuid4())
-        mock_flr_normal.read_1 = "gds://volume/path/normal_read_1.fastq.gz"
-        mock_flr_normal.save()
-
-        mock_flr_tumor = FastqListRow()
-        mock_flr_tumor.lane = 2
-        mock_flr_tumor.rglb = TestConstant.library_id_tumor.value
-        mock_flr_tumor.rgsm = TestConstant.sample_id.value
-        mock_flr_tumor.rgid = str(uuid.uuid4())
-        mock_flr_tumor.read_1 = "gds://volume/path/tumor_read_1.fastq.gz"
-        mock_flr_tumor.read_2 = "gds://volume/path/tumor_read_2.fastq.gz"
-        mock_flr_tumor.save()
+        build_tn_mock()
 
         logger.info("-" * 32)
         when(orchestrator.demux_metadata).handler(...).thenReturn([
@@ -302,6 +309,20 @@ class OrchestratorUnitTests(PipelineUnitTestCase):
         logger.info("-" * 32)
         self.assertIsNotNone(result)
         logger.info(f"Orchestrator lambda call output: \n{json.dumps(result)}")
+
+    def test_create_tn_job(self):
+        """
+        python manage.py test data_processors.pipeline.tests.test_orchestrator.OrchestratorUnitTests.test_create_tn_job
+        """
+
+        build_tn_mock()
+
+        job_dict = orchestrator.create_tn_job(tn_mock_subject_id)
+
+        self.assertEqual(job_dict['subject_id'], tn_mock_subject_id)
+        self.assertEqual(job_dict['fastq_list_rows'][0]['rglb'], TestConstant.library_id_normal.value)
+        self.assertEqual(job_dict['fastq_list_rows'][0]['read_1']['location'], tn_mock_normal_read_1)
+        logger.info(f"Job JSON: {json.dumps(job_dict)}")
 
 
 class OrchestratorIntegrationTests(PipelineIntegrationTestCase):
