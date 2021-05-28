7d4bb6499568218ec61bea207ad2efab374698bc
Victor San Kho Lin <victor@sankholin.com>
2021-05-28 22:59:37 +1000

    Added OpenAPI Swagger generator
    * Bump dependencies

--- data_portal/settings/base.py
+++ data_portal/settings/base.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'data_processors',
     'utils',
     'aws_xray_sdk.ext.django',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- data_portal/urls.py
+++ data_portal/urls.py
@@ -1,4 +1,8 @@
+from django.conf.urls import url
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 from data_portal import views
 from .routers import OptionalSlashDefaultRouter
@@ -16,7 +20,23 @@ router.register(r'runs', RunViewSet, basename='runs')
 router.register(r'reports', ReportViewSet, basename='reports')
 router.register(r'presign', PresignedUrlViewSet, basename='presign')
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="UMCCR Data Portal API",
+      default_version='v1',
+      description="UMCCR Data Portal API",
+      terms_of_service="https://umccr.org/",
+      contact=openapi.Contact(email="services@umccr.org"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('files', views.search_file, name='file-search'),
     path('file-signed-url', views.sign_s3_file, name='file-signed-url'),
     path('storage-stats', views.storage_stats, name='storage-stats'),
