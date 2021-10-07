e16348ec303b119e282720cb2f780185b8bf7bab
William <Wintan@student.unimelb.edu.au>
2021-10-08 01:35:24 +1100

    add unit test

---
+++ data_portal/tests/test_library_run.py
@@ -0,0 +1,53 @@
+import logging
+
+from django.test import TestCase
+from data_portal.models import LibraryRun
+from django.core.exceptions import ObjectDoesNotExist
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+class SequenceTestCase(TestCase):
+    def setUp(self):
+        logger.info('Create Object data')
+        LibraryRun.objects.create(
+            library_id="L2000001",
+            instrument_run_id="191213_A00000_00000_A000000000",
+            run_id="r.AAAAAAAAA",
+            lane=2,
+            override_cycles="",
+            coverage_yield="",
+            qc_pass=True,
+            qc_status="good",
+            valid_for_analysis=True
+        )
+        LibraryRun.objects.create(
+            library_id="L2000002",
+            instrument_run_id="191213_A00000_00000_A000000000",
+            run_id="r.AAAAAAAAA",
+            lane=2,
+            override_cycles="",
+            coverage_yield="",
+            qc_pass=True,
+            qc_status="poor",
+            valid_for_analysis=True
+        )
+
+    def test_get_sequence(self):
+
+        logger.info("Test get success sequence table")
+        get_complete_sequence = LibraryRun.objects.get(library_id='L2000002')
+        self.assertEqual(get_complete_sequence.library_id, 'L2000002', 'Correct Library ID fetch is expected')
+
+
+    def test_get_api(self):
+
+        # Get sequencee list
+        logger.info('Get sequence API')
+        response = self.client.get('/libraryrun/')
+        self.assertEqual(response.status_code, 200, 'Ok status response is expected')
+
+        logger.info('Check if API return result')
+        result_response = response.data['results']
+        self.assertGreater(len(result_response), 0, 'A result is expected')
