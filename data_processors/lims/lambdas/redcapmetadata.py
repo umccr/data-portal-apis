6f87916fce288e67c11b0fbc6fd02684fd1d12ac
Andrei Seleznev <aseleznev@illumina.com>
2021-10-14 16:47:48 +1100

    redcapmetadata now fetches by library id and performs metadata lookups as well as pierandx queries

--- data_processors/lims/lambdas/redcapmetadata.py
+++ data_processors/lims/lambdas/redcapmetadata.py
@@ -18,10 +18,12 @@ import pandas as pd
 
 from data_processors.lims.services import redcapmetadata_srv
 from utils import libjson
+from data_portal.models import LabMetadata
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+#TODO we might want to rename this lambda e.g. not recapmetadata but pierianmetadata
 
 def _halt(msg):
     logger.error(msg)
@@ -29,7 +31,78 @@ def _halt(msg):
         'message': msg
     }
 
+
+def handler_pierian_metadata_by_library_id(event, context):
+    """
+    given a list of library IDs, get their subject IDs from LabMetadata & fetch RedCap info needed for Pieran DX
+
+    event payload dict
+    {
+        library_ids = ['some','library','ids']
+    }
+    Handler for Pierian metadata specific fetching 
+
+    :param event:
+    :param context:
+    :return: dataframe requested redcap metadata
+    """
+
+    logger.info("Start processing RedCap database request update event")
+    logger.info(libjson.dumps(event))
+
+    requested_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    logger.info(f"Fetching data from RedCap at {requested_time}")
+
+    library_ids = event.get('library_ids')
+
+    if not isinstance(library_ids, list):
+        _halt(f"Payload error. Must be list. Found: {type(list)}")
+
+    for k in library_ids:
+        if not isinstance(k, str):
+            _halt(f"Payload error. Must be list of strings. found lit of: {type(k)}")
+    
+    lms = LabMetadata.objects.filter(library_id__in=library_ids)
+    if not lms:
+        return pd.DataFrame({})
+
+    values_in = { 'subjectid' : [d.subject_id for d in lms] }   # will search for these in redcap
+    df = pd.DataFrame( { 'subject_id' : [d.subject_id for d in lms] , 'library_id' : [d.library_id for d in lms] } )  # TODO check if iteration order always the same, ELSE transfer to preserved-order structure
+
+    #   grab neccessary columns from redcap as a df, merge them in
+    col_redcap_pierian_mappings = {"sub_biopsy_date":  "Date collected", "subjectid": "Subject Id","enr_patient_enrol_date":"enr_patient_enrol_date","createdate":"createdate","req_request_date":"req_request_date","report_date":"report_date","req_diagnosis":"req_diagnosis" }
+    df_redcap = redcapmetadata_srv.retrieve_metadata(values_in,col_redcap_pierian_mappings.keys())
+    df_redcap = df_redcap.rename(columns = { "subjectid": "subject_id" })
+    df = df.merge(df_redcap, on='subject_id', how='right')
+
+    # rename cols to Pieran DX sheet names TODO figure these out, also consolifate
+    df = df.rename(columns = col_redcap_pierian_mappings)   
+    df = df.rename(columns = { "library_id": "Library Id" , "subject_id": "Subject Id" })
+    
+    # add default fields
+    df['Sample Type'] = "Validation Sample"
+    df['Specimen Type'] = 119361006
+    df['Patient First Name'] = 'n/a'
+    df['Patient Last Name'] = 'n/a'
+    df['Patient Date Of Birth'] = 'n/a'
+    df['Race'] = 'n/a'
+    df['Medical Record Numbers'] = 'n/a'
+    df['Number of unstable microsatellite loci'] = 'n/a'
+    df['Usable MSI Sites'] = 'n/a'
+    df['Tumor Mutational Burden (Mutations/Mb)'] = 'n/a'
+    df['Percent Unstable Sites'] = 'n/a'
+    df['Percent Tumor Cell Nuclei in the Selected Areas'] = 'n/a'
+    df['Participant ID'] = df["Subject Id"]
+    df['Accession Number'] = df["Subject Id"] + "_" + df["Library Id"]
+    df['External Specimen ID'] = df.apply (lambda row: lms.filter(library_id__iexact=row["Library Id"])[0].external_subject_id, axis=1)
+
+    #TODO drop subject and librardy ID fields ?
+    return df
+
+
 def handler(event, context): 
+    # basic handler for generic redcap retriecal, not actually used 
+    ### TODO delete?
     """event payload dict
     {
         'values_in':  { "keyname" : list('the','values') },
@@ -61,10 +134,17 @@ def handler(event, context):
         columns = list()
     return redcapmetadata_srv.retrieve_metadata(values_in,columns)
 
-def handler_pierian_metadata(event, context):
-    """event payload dict
+
+def handler_pierian_metadata_only(event, context):
+    # slightly more complex handler , pieran specific, but no labmetadata retrieval
+    ### TODO delete?
+
+    """
+    grabs metadata from pieran but ignored labmetadata 
+    event payload dict
     {
         'values_in':  { "keyname" : list('the','values') } 
+        #subject_library_ids : [ { "subject_id": "xxx", "library_id": "xxx" }, { "subject_id": "xxx", "library_id": "xxx" } ]
     }
     Handler for Pierian metadata specific fetching 
 
@@ -88,37 +168,6 @@ def handler_pierian_metadata(event, context):
         if not isinstance(v, list):
             _halt(f"Payload error. Must be dict with str keys and list values. Value found: {type(v)}")
     
-
-    """ Metadatra needed for Pierian:
-    ================
-    Sample Type  - default this to Validation Sample
-    Indication - sheet says CancerType, not present 
-    Disease  - '?'
-    Is Identified  study_subcategory
-    Requesting Physicians First Name    ?
-    Requesting Physicians Last Name ?
-    Accession Number    tracking sheet (subectid,libraryid), is this in RedCap
-    Patient First Name  n/a
-    Patient Last Name   n/a
-    Patient Date Of Birth   n/a
-    Study ID    ?
-    Participant ID  trackingsheet StudyID
-    Specimen Type   119361006   
-    External Specimen ID    trackingsheet ExternalSubjectID
-    Date Accessioned    timestamp of data uplopad to s3 bucket (does this then need to be provided to the lambda call?)
-    Date collected  sub_biopsy_date
-    Date Received   ?
-    Gender  ?
-    Ethnicity   ?
-    Race    n/a
-    Medical Record Numbers  n/a
-    Hospital Numbers    ?
-    Number of unstable microsatellite loci
-    Usable MSI Sites
-    Tumor Mutational Burden (Mutations/Mb) n/a
-    Percent Unstable Sites n/a
-    Percent Tumor Cell Nuclei in the Selected Areas n/a
-    """
 
     col_redcap_pierian_mappings = {"sub_biopsy_date":  "Date collected" } #, "Indication": "cancer_type"  } # this ought to have the field we're searchig on in values_in
     df = redcapmetadata_srv.retrieve_metadata(values_in,col_redcap_pierian_mappings.keys())
--- data_processors/lims/lambdas/tests/test_redcapmetadata.py
+++ data_processors/lims/lambdas/tests/test_redcapmetadata.py
@@ -10,7 +10,7 @@ from redcap import Project
 from data_processors.lims.lambdas import redcapmetadata
 from data_processors.lims.services import redcapmetadata_srv
 from data_processors.lims.tests.case import LimsIntegrationTestCase, logger
-
+from data_portal.models import LabMetadata
 
 mock_sheet_1 = b"""recordid,redcap_event_name,redcap_repeat_instrument,redcap_repeat_instance,redcap_data_access_group,redcap_survey_identifier,enrolment_timestamp,enr_email,study_category,study_name,study_column_sub,study_name_research,study_name_research_other,study_subcategory,study_name_derived,subjectid,man_sbj,enr_patient_studyid,enr_patient_urn_local,enr_patient_urn_other,enr_patient_enrol_date,enr_patient_firstname,enr_patient_surname,enr_patient_dob,enr_patient_age,enr_patient_sex,enr_patient_ethnicity,enr_patient_race,enr_patient_address_line1,enr_patient_address_line2,enr_patient_address_suburb,enr_patient_address_state,enr_patient_address_postcode,enr_patient_phone,enr_patient_email,enr_patient_consent_column,enr_patient_consent_other,enr_patient_consent_date_other,enr_linked_subject,enr_linked_id_valid,enr_linked_recordid,enr_linked_subject2,enr_linked_id_valid2,enr_linked_recordid2,enr_comments,enr_create_user,enr_create_dt,createdate,createyear,createyear2,yearprefix,enrolment_complete,request_timestamp,req_request_date,req_requester_name,req_requester_email,req_requester_inst,req_requester_inst_other,req_clin_provider,req_clinprov_name,req_clinprov_email,req_pi,req_pi_name,req_pi_email,req_pi_institution,req_pi_institution_other,req_project,req_custodian_name,req_custodian_email,req_tumour_stream,req_tuscat_brn,req_tuscat_brt,req_tuscat_lgi,req_tuscat_ugi,req_tuscat_gu,req_tuscat_gyn,req_tuscat_hem,req_tuscat_hn,req_tuscat_lng,req_tuscat_ped,req_tuscat_sar,req_tuscat_skn,req_tuscat_pedspec,req_tuscat_oth,req_tuscat_calc,req_diagnosis,req_seq_type___1,req_seq_type___2,req_seq_type___3,req_seq_type___4,req_seq_type___99,req_seq_type_other,req_seq_reason___1,req_seq_reason___2,req_seq_reason___3,req_seq_reason___99,req_seq_reason_other,req_return_fastq,req_return_fastq_name,req_return_fastq_email,req_return_keybaseid,req_copy_clinician,req_copy_clinician1_name,req_copy_clinician1_email,req_copy_clinician2_name,req_copy_clinician2_email,req_comments,req_create_user,req_create_dt,request_complete,app_pi_name,app_pi_name_other,app_pi_email_other,app_pi_approved,app_approval_date,app_comments,app_create_user,app_create_dt,approval_complete,consent_patient_timestamp,cons_completedby,cons_2,cons_3,cons_4,cons_rel_name,cons_rel_relation,cons_rel_dob,cosn_rel_address,cons_rel_phone,cons_6,cons_7,cons_8,cons_9,cons_participant_name,cons_participant_signature,cons_participant_sig_date,cons_scanned_form,cons_witness_name,cons_witness_signature,cons_witness_sig_date,cons_consent_date,cons_create_user,cons_create_dt,consent_patient_complete,consent_pi_timestamp,conspi_pi_name,conspi_pi_signature,conspi_pi_sig_date,conspi_merged_file,conspi_create_user,conspi_create_dt,consent_pi_complete,clinical_phenotype_timestamp,clin_ecog_qsorres,clin_rsorres,clin_primtumloc,clin_ctx,clin_ctx_type,clin_ctx_num,clin_ctx_prior,clin_rtx,clin_genetic_prior,clin_genetic_testname,clin_genetic_germline,clin_genetic_germlinename,clin_genetic_somatic,clin_genetic_somaticname,clin_seq_concur,clin_seq_concurname,clin_history,clin_clinfile1,clin_clinfile2,clin_histopathfile1,clin_histopathfile2,clin_comments,clin_create_user,clin_create_dt,clinical_phenotype_complete,biopsy_sample_type_timestamp,sub_subtiming,sub_subtype___1,sub_subtype___2,sub_archival_fresh,sub_sample_type___1,sub_sample_type___2,sub_sample_type___3,sub_sample_type___4,sub_sample_type___5,sub_sample_type___6,sub_sample_type___7,sub_sample_type___8,sub_sample_type___99,sub_sample_type_other,sub_ffpe_custodiallab,sub_ffpe_identifier,sub_ffpe_recall,sub_biopsy_date,sub_biopsy_time,sub_biopsy_site,sub_biopsy_type,sub_biopsy_type_other,sub_blood_date,sub_germlinesample_type___1,sub_germlinesample_type___2,sub_germlinesample_type___3,sub_germlinesample_type___4,sub_germlinesample_type___5,sub_germlinesample_type___99,sub_germlinesample_type_other,sub_comments,sub_create_user,sub_create_dt,biopsy_sample_type_complete,tdna_id,tdna_id_external,tdna_starlims,tdna_starlimsid,tdna_mdxprj,tdna_mdxid,tdna_prjid,tdna_sample_source,sub_source_number,tdna_receivedate,tdna_idmatch,tdna_identifiers_diff,tdna_extraction,tdna_extractiondate,tdna_photo,tdna_photo2,tdna_submissionform,tdna_submissionform2,tdna_qcdate,tdna_nanodrop,tdna_dilfactor,tdna_qubit,tdna_stockconc,tdna_integrity,tdna_gapdh,tdna_passqc,tdna_qcwhyfail,tdna_qcwhyfail_other,tdna_resub_email,tdna_cfdna,tdna_cfdna_total,tdna_libprepdate,tdna_batchid,tdna_libraryid,tdna_libprep,tdna_libprep_whyfail,tdna_libprep_whyfail_other,tdna_seqdate,tdna_seq_success,tdna_seq_whyfail,tdna_seq_whyfail_other,tdna_topup,tdna_topupdate,tdna_topup_libraryid,tdna_samplereturned,tdna_samplereturn_date,tdna_comments,tdna_create_user,tdna_create_dt,lab_tumour_dna_complete,trna_id,trna_id_external,trna_starlims,trna_starlimsid,trna_mdxprj,trna_mdxid,trna_prjid,trna_sample_source,sub_source_number_278db1,trna_receivedate,trna_idmatch,trna_identifiers_diff,trna_extraction,trna_extractiondate,trna_photo,trna_photo2,trna_submissionform,trna_submissionform2,trna_qcdate,trna_nanodrop,trna_dilfactor,trna_qubit,trna_stockconc,trna_integrity,trna_gapdh,trna_passqc,trna_qcwhyfail,trna_qcwhyfail_other,trna_resub_email,trna_libprepdate,trna_batchid,trna_libraryid,trna_libprep,trna_libprep_whyfail,trna_libprep_whyfail_other,trna_seqdate,trna_seq_success,trna_seq_whyfail,trna_seq_whyfail_other,trna_topup,trna_topupdate,trna_topup_libraryid,trna_samplereturned,trna_samplereturn_date,trna_comments,trna_create_user,trna_create_dt,lab_tumour_rna_complete,gdna_id,gdna_id_external,gdna_starlims,gdna_starlimsid,gdna_mdxprj,gdna_mdxid,gdna_prjid,gdna_sample_source,sub_source_number_2c4175,gdna_receivedate,gdna_idmatch,gdna_identifiers_diff,gdna_extraction,gdna_extractiondate,gdna_photo,gdna_photo2,gdna_submissionform,gdna_submissionform2,gdna_qcdate,gdna_nanodrop,gdna_dilfactor,gdna_qubit,gdna_stockconc,gdna_integrity,gdna_gapdh,gdna_passqc,gdna_qcwhyfail,gdna_qcwhyfail_other,gdna_resub_email,gdna_libprepdate,gdna_batchid,gdna_libraryid,gdna_libprep,gdna_libprep_whyfail,gdna_libprep_whyfail_other,gdna_seqdate,gdna_seq_success,gdna_seq_whyfail,gdna_seq_whyfail_other,gdna_topup,gdna_topupdate,gdna_topup_libraryid,gdna_samplereturned,gdna_samplereturn_date,gdna_comments,gdna_create_user,gdna_create_dt,lab_germline_dna_complete,rnasum_ref,snomed_code,seq_fail,seq_failreason,seq_fail_reason_other,seq_fail_report,seq_fail_report_other,seq_fail_email,seq_coverage_tumour,seq_coverage_normal,sample_quality___1,sample_quality___2,sample_quality___3,sample_quality___4,ploidy,tumour_purity,polyclonal_proportion,mutations_mbase,coding_variants,snv_number,indels_number,variants_tier1,variants_tier2,variants_tier3,variants_tier4,pathogen,circosplot,genome_integrity,mutator_type,signature,msi_status,tissueorigin,md_germlinecurr,md_germlinecurrname,md_somaticcurr,md_somaticcurrname,report_yesno,report_date,report_file,mol_create_user,mol_create_dt,molecular_data_reporting_complete,clinical_follow_up_timestamp,fup_eventdate,fup_vs,dthdat_dsyn,dthdat,alivedat,newtxyn,cmtrt1,cmdur1,cm1_rsorres,cmtrt2,cmdur2,cm2_rsorres,cmtrt3,cmdur3,cm3_rsorres,cmtrt4,cmdur4,cm4_rsorres,cmtrt5,cmdur5,cm5_rsorres,mngimpt,cm_mngimpt___1,cm_mngimpt___2,cm_mngimpt___3,cm_mngimpt___4,cm_mngimpt___5,trial_mngimpt,trialphase_mngimpct,trialid_mngimpct,capyn,cm_cap___1,cm_cap___2,cm_cap___3,cm_cap___4,cm_cap___5,useful_mngimpct,detail_mngimpct___1,detail_mngimpct___2,detail_mngimpct___3,detail_mngimpct___4,detail_mngimpct___5,detail_mngimpct___6,detail_mngimpct___7,detail_mngimpct___99,detailoth_mngimpct,fup_fccref,fup_create_user,fup_create_dt,clinical_follow_up_complete,subject_status_summary_complete,withdrawal_patient_timestamp,withdrawal_completed_by,participant_name_v2,participant_signature_v2,participant_signature_date_v2,participant_withdrawal_scanned,wdr_create_user,wdr_create_dt,withdrawal_patient_complete,withdrawal_pi_timestamp,withdrawal_pi_name,withdrawal_pi_signature,withdrawal_pi_signature_date,withdrawal_final,wdrp_create_user,wdrp_create_dt,withdrawal_pi_complete,fhirtest,todo_dev_complete
 5001,enrolment_arm_1,,,_stafford_fox,,,,1,6,,,,3,"Stafford Fox",SBJ05001,,SFRC01442,MEH745025,SVP745025,2021-09-09,,,,,1,7,7,,,,,,,,,1,2021-08-03,,1,0,,0,0,"This is a deidentified sample",grant.lee,"2021-09-12 14:43:41",2021-09-12,21,21,,2,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
@@ -60,7 +60,27 @@ mock_sheet_2 = b""""Record ID","Event Name","Repeat Instrument","Repeat Instance
 5001,"Request Original","Biopsy Sample Type",1," - Stafford Fox",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"1st submission",Checked,Checked,Yes,Unchecked,Unchecked,Unchecked,Unchecked,Checked,Unchecked,Unchecked,Unchecked,Unchecked,,,,,2021-07-30,,"Left tonsil","Core biopsy",,2021-08-02,Unchecked,Unchecked,Checked,Unchecked,Unchecked,Unchecked,,"Comments for sample submission go here.",grant.lee,"2021-09-12 14:54:04",Complete,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"""
 
 class RedcapUnitTests(TransactionTestCase):
-    def test_handler(self):
+    def test_handler_by_library_id(self):
+        """
+        python manage.py test data_processors.lims.lambdas.tests.test_redcapmetadata.RedcapUnitTests.test_handler_by_library_id
+        """
+        mock_csv = tempfile.NamedTemporaryFile(suffix='.csv', delete=True)
+        mock_csv.write(mock_sheet_1.lstrip().rstrip())
+        mock_csv.seek(0)
+        mock_csv.flush()
+        when(redcapmetadata_srv).download_redcap_project_data().thenReturn(pd.read_csv(mock_csv))
+        make_mock_labmeta()
+
+        result = redcapmetadata.handler_pierian_metadata_by_library_id({
+                'library_ids': [ "L2101081","L2101080","L2101089","L2101081_topup" ]
+        }, None)
+
+        logger.info("-" * 32)
+        logger.info("Example output:")
+        logger.info((result.to_string()))
+
+    def test_handler_simple(self):
+        ## TODO no use for simple case, delete?
         """
         python manage.py test data_processors.lims.lambdas.tests.test_redcapmetadata.RedcapUnitTests.test_handler
         """
@@ -70,7 +90,7 @@ class RedcapUnitTests(TransactionTestCase):
         mock_csv.flush()
         when(redcapmetadata_srv).download_redcap_project_data().thenReturn(pd.read_csv(mock_csv))
 
-        result = redcapmetadata.handler({
+        result = redcapmetadata.handler_pierian_metadata({
                 'values_in': { 'subjectid': ["SBJ05001","SBJ05006","NotReal"] },
                 'columns': ["subjectid","enr_patient_enrol_date","createdate","req_request_date","report_date","req_diagnosis"]
         }, None)
@@ -79,23 +99,96 @@ class RedcapUnitTests(TransactionTestCase):
         logger.info("Example output:")
         logger.info((result))
 
-    def test_handler_pierian_columns(self):
-        """
-        test with the columns needed by PierianDX
-        python manage.py test data_processors.lims.lambdas.tests.test_redcapmetadata.RedcapUnitTests.test_handler_pierian_columns
-        """
-        mock_csv = tempfile.NamedTemporaryFile(suffix='.csv', delete=True)
-        mock_csv.write(mock_sheet_1.lstrip().rstrip())
-        mock_csv.seek(0)
-        mock_csv.flush()
-        when(redcapmetadata_srv).download_redcap_project_data().thenReturn(pd.read_csv(mock_csv))
+def make_mock_labmeta():
+    
+        # make a duplicate to test update, its phenotype is normal but in sheet it is tumor
+        lab_meta = LabMetadata(
+            library_id="L2101080",
+            sample_name="SAMIDA-EXTSAMA",
+            sample_id="SAMIDA",
+            external_sample_id="EXTSAMA",
+            subject_id="SBJ05006",
+            external_subject_id="EXTSUBID_A",
+            phenotype="NORMAL",
+            quality="good",
+            source="FFPE",
+            project_name="Foo",
+            project_owner="Roo",
+            experiment_id="Exper1",
+            type="WTS",
+            assay="NebRNA",
+            override_cycles="Y151;I8;I8;Y151",
+            workflow="clinical",
+            coverage="6.0",
+            truseqindex="H07"
+        )
+        lab_meta.save()
 
+        
+        # make a duplicate to test update, its phenotype is normal but in sheet it is tumor
+        lab_meta = LabMetadata(
+            library_id="L2101081",
+            sample_name="SAMIDB-EXTSAMB",
+            sample_id="SAMIDB",
+            external_sample_id="EXTSAMB",
+            subject_id="SBJ05001",
+            external_subject_id="EXTSUBIDB",
+            phenotype="NORMAL",
+            quality="poor",
+            source="FFPE",
+            project_name="Fake",
+            project_owner="Bob",
+            experiment_id="Exper1",
+            type="WTS",
+            assay="NebRNA",
+            override_cycles="Y151;I8;I8;Y151",
+            workflow="clinical",
+            coverage="6.0",
+            truseqindex="H07"
+        )
+        lab_meta.save()
 
-        result = redcapmetadata.handler_pierian_metadata({
-                'values_in': { 'subjectid': ["SBJ05001","SBJ05006","NotReal"] }
-        }, None)
-       
- 
-        logger.info("-" * 32)
-        logger.info("Example output:")
-        logger.info((result))
+           
+        # make a duplicate to test update, its phenotype is normal but in sheet it is tumor
+        lab_meta = LabMetadata(
+            library_id="L2101081_topup",
+            sample_name="SAMIDB-EXTSAMB",
+            sample_id="SAMIDB",
+            external_sample_id="EXTSAMB",
+            subject_id="SBJ05001",
+            external_subject_id="EXTSUBIDB",
+            phenotype="NORMAL",
+            quality="poor",
+            source="FFPE",
+            project_name="Fake",
+            project_owner="Bob",
+            experiment_id="Exper1",
+            type="WTS",
+            assay="NebRNA",
+            override_cycles="Y151;I8;I8;Y151",
+            workflow="clinical",
+            coverage="6.0",
+            truseqindex="H07"
+        )
+        lab_meta.save()
+
+        lab_meta = LabMetadata(
+            library_id="L2101089",
+            sample_name="SAMIDAAAA-EXTSAMAAAA",
+            sample_id="SAMIDAASAA",
+            external_sample_id="EXTSAMAAAA",
+            subject_id="SBJ05005",
+            phenotype="NORMAL",
+            quality="good",
+            source="FFPE",
+            project_name="Foo",
+            project_owner="Roo",
+            experiment_id="Exper1",
+            type="WTS",
+            assay="NebRNA",
+            override_cycles="Y151;I8;I8;Y151",
+            workflow="clinical",
+            coverage="6.0",
+            truseqindex="H07"
+        )
+        lab_meta.save()
