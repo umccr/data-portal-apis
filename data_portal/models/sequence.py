fc8afe13e24c021672457b93aa7d83282da9736c
William <Wintan@student.unimelb.edu.au>
2021-10-27 10:54:08 +1100

    Implement filter_object_by_parameter_keyword in SequenceManager

--- data_portal/models/sequence.py
+++ data_portal/models/sequence.py
@@ -1,7 +1,10 @@
 import logging
+from django.core.exceptions import FieldError
 
 from django.db import models
 from django.db.models import QuerySet
+
+from .utils import filter_object_by_parameter_keyword
 
 logger = logging.getLogger(__name__)
 
@@ -51,45 +54,12 @@ class SequenceManager(models.Manager):
     def get_by_keyword(self, **kwargs) -> QuerySet:
         qs: QuerySet = self.all()
 
-        instrument_run_id = kwargs.get('instrument_run_id', None)
-        if instrument_run_id:
-            qs = qs.filter(instrument_run_id__iexact=instrument_run_id)
-
-        run_id = kwargs.get('run_id', None)
-        if run_id:
-            qs = qs.filter(run_id__iexact=run_id)
-
-        sample_sheet_name = kwargs.get('sample_sheet_name', None)
-        if sample_sheet_name:
-            qs = qs.filter(sample_sheet_name__iexact=sample_sheet_name)
-
-        gds_folder_path = kwargs.get('gds_folder_path', None)
-        if gds_folder_path:
-            qs = qs.filter(gds_folder_path__iexact=gds_folder_path)
-
-        gds_volume_name = kwargs.get('gds_volume_name', None)
-        if gds_volume_name:
-            qs = qs.filter(gds_volume_name__iexact=gds_volume_name)
-
-        reagent_barcode = kwargs.get('reagent_barcode', None)
-        if reagent_barcode:
-            qs = qs.filter(reagent_barcode__iexact=reagent_barcode)
-
-        flowcell_barcode = kwargs.get('flowcell_barcode', None)
-        if flowcell_barcode:
-            qs = qs.filter(flowcell_barcode__iexact=flowcell_barcode)
-
-        status = kwargs.get('status', None)
-        if status:
-            qs = qs.filter(status__iexact=status)
-
-        start_time = kwargs.get('start_time', None)
-        if start_time:
-            qs = qs.filter(start_time__iexact=start_time)
-
-        end_time = kwargs.get('end_time', None)
-        if end_time:
-            qs = qs.filter(end_time__iexact=end_time)
+        keywords = kwargs.get('keywords', None)
+        if keywords:
+            try:
+                qs = filter_object_by_parameter_keyword(qs,keywords)
+            except FieldError:
+                qs = self.none()
 
         return qs
 
