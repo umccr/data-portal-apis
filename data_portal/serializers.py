d38478c2c6de174753afc8790c522771197cad03
Victor San Kho Lin <victor@sankholin.com>
2022-02-14 16:59:50 +1100

    Added also 2019 hint to LabMetadataSyncSerializer

--- data_portal/serializers.py
+++ data_portal/serializers.py
@@ -164,7 +164,7 @@ class S3ObjectSerializer(serializers.Serializer):
 
 
 class LabMetadataSyncSerializer(serializers.Serializer):
-    sheets = serializers.ListField(default=['2020', '2021', '2022'])
+    sheets = serializers.ListField(default=['2019', '2020', '2021', '2022'])  # OpenAPI swagger doc hint only
     truncate = serializers.BooleanField(default=True)
 
     def update(self, instance, validated_data):
