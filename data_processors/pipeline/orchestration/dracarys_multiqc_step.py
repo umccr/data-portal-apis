8f4ea293d7cdc7f43b0d232927272115390ea72d
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2023-03-03 15:05:56 +1100

    Cleanup Dracarys MultiQC step, perhaps needs to be generalised to Dracarys_Step instead and handle multiple types of inputs instead of creating different steps for different types of inputs from S3 buckets. Also remove all GDS-related logic since Dracarys takes care of this already.

--- data_processors/pipeline/orchestration/dracarys_multiqc_step.py
+++ data_processors/pipeline/orchestration/dracarys_multiqc_step.py
@@ -1,20 +1,11 @@
 # -*- coding: utf-8 -*-
-import json
 import logging
 from typing import List, Dict
 
-from libumccr.aws import libssm, libsqs
-from libica.openapi import libgds
-from libica.app import configuration
-#from data_processors.lims.lambdas.pierianutils import ica_gds, globals
+import awswrangler as wr
 
-from libica.openapi.libgds import FileResponse
-from urllib.parse import urlparse
-
-from data_portal.models.labmetadata import LabMetadata
 from data_portal.models.workflow import Workflow
 from data_processors.pipeline.domain.config import SQS_DRACARYS_QUEUE_ARN, S3_DRACARYS_BUCKET_NAME
-from data_processors.pipeline.services import metadata_srv
 from data_processors.pipeline.tools import liborca
 
 logger = logging.getLogger(__name__)
@@ -23,98 +14,24 @@ logger.setLevel(logging.INFO)
 
 def perform(this_workflow: Workflow):
     output_json = this_workflow.output
+    if output_json is None:
+        raise RuntimeError('Missing workflow information')
     
     try:
         lookup_keys = ['multiqc_output_directory']
         multiqc_output_directory = liborca.parse_workflow_output(output_json, lookup_keys)
         outprefix = multiqc_output_directory['nameroot']
-        multiqc_files = collect_gds_multiqc_json_files(multiqc_output_directory['location'])
+        multiqc_files = wr.s3.list_objects(S3_DRACARYS_BUCKET_NAME) # TODO: Work out prefix handling
     except KeyError as e:
         logging.info("Dracarys multiqc step didn't find expected multiqc output data. Exiting.")
         return {}
-    
-    jobs_list = []
-
-    for multiqc_file in multiqc_files:
-        presignurl = get_presign_url_for_single_file(multiqc_file)
-
-        queue_arn=libssm.get_ssm_param(SQS_DRACARYS_QUEUE_ARN)
-
-        job = {  "presign_url_json": presignurl, "output_prefix": outprefix, "target_bucket_name": libssm.get_ssm_param(S3_DRACARYS_BUCKET_NAME) }
-        jobs_list.append(job) 
-
-        libsqs.dispatch_jobs(
-                queue_arn=queue_arn,
-                job_list=jobs_list,
-            )
-    
-    if not jobs_list:
-        return {}
-
-    return {
-        "dracarys_multiqc_step": jobs_list
-    }
 
 
-def get_presign_url_for_single_file(multiqc_file):
-    path = multiqc_file.path
-    volume_name = multiqc_file.volume_name
-
-    with libgds.ApiClient(configuration(libgds)) as api_client:
-        files_api = libgds.FilesApi(api_client)
-        try:
-            file_list: libgds.FileListResponse = files_api.list_files(
-                volume_name=[volume_name],
-                path=[path],
-                page_size=1000,
-                include="presignedUrl"
-            )
-
-            if len(file_list.items) != 1:
-                logger.error(f"Expected exactly one file at gds://" + volume_name + "/" + path)
-                raise FileNotFoundError
-
-            presignurl = file_list.items[0].presigned_url
-
-            # while end
-        except libgds.ApiException as e:
-            logger.error(f"Exception when calling list_files: \n{e}")    
-    return presignurl
-
-
-
-def collect_gds_multiqc_json_files(location: str):
-    multiqc_json_files = []
-    volume_name, path_ = parse_gds_path(location)
-
-    with libgds.ApiClient(configuration(libgds)) as api_client:
-        files_api = libgds.FilesApi(api_client)
-        try:
-            page_token = None
-            while True:
-                file_list: libgds.FileListResponse = files_api.list_files(
-                    volume_name=[volume_name],
-                    path=[f"{path_}/*"],
-                    page_size=1000,
-                    page_token=page_token,
-                )
-                for item in file_list.items:
-                    file: libgds.FileResponse = item
-
-                    if file.name.endswith('multiqc_data.json'):
-                        multiqc_json_files.append(file)
-
-                page_token = file_list.next_page_token
-                if not file_list.next_page_token:
-                    break
-            # while end
-
-        except libgds.ApiException as e:
-            logger.error(f"Exception when calling list_files: \n{e}")
-    return multiqc_json_files
-
-def parse_gds_path(gds_path):
-    path_elements = gds_path.replace("gds://", "").split("/")
-    volume_name = path_elements[0]
-    path_ = f"/{'/'.join(path_elements[1:])}"
-    return volume_name, path_
+def persist_dracarys_data(path: str):
+    ''' Takes Dracarys output files on S3 and turns them into a Dataframe,
+        ready to be consumed by the portal DB
+    '''
+    if 'tsv' or 'csv' in path:
+        wr.s3.read_csv(path)
+    elif 'parquet' in path:
+        wr.s3.read_parquet(path)
